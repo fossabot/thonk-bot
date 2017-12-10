382ac14e1cdfb3adb7fc10b303b26e018e8609fc
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-10 21:22:11 +0000

    added config, woohoo!

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const discord = require('discord.js'); //add discord.js idk
 const client = new discord.Client();
 const fs = require('fs');
+const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+bot.on("ready", function() {
+  console.log('Ready!');
+})
 
 client.on('message', (message) => {
   if(!message.guild) return; //cuz we dont want dm
@@ -11,7 +16,7 @@ client.on('message', (message) => {
     if(message.content == 'whos the best pony?') {
       message.reply('Celestia is da best pony!!/!!!!!');
     }
-    if(message.content == '!pony') {
+    if(message.content == config.prefix + 'pony') {
       var voiceChannel = message.member.voiceChannel;
       voiceChannel.join().then(connection =>{
         const dispatcher = connection.playFile('./media/shit.mp3') //this is a fucking mlp theme song if you wondering
@@ -21,7 +26,7 @@ client.on('message', (message) => {
         })
       })
         }
-        if(message.content == '!cringe') {
+        if(message.content == config.prefix + 'cringe') {
           var fs = require('fs');
           var cringe = fs.readFileSync('cringe.txt').toString().split("\n");
           var rnd = Math.floor(Math.random() * 21);

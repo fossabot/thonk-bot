4b0bca8e39a4f1e3b1d65dd23a7be7206704a308
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-10 17:18:30 +0000

    added a worst message ever

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ client.on('message', (message) => {
     if(message.content == 'kys') {
       message.reply('Alright, thats really rude.');
     }
+
+    if(message.content == 'whos the best pony?') {
+      message.reply('Celestia is da best pony!!/!!!!!')
+    }
 });
 
 client.login(process.env.BOT_TOKEN); //make the bot appear

a4eef823aba353793092722e3811e257b3078f23
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-10 16:45:04 +0000

    changed and add lots of things

---
+++ index.js
@@ -0,0 +1,10 @@
+const discord = require('discord.js'); //add discord.js idk
+const client = new discord.Client();
+
+client.on('message', (message) => {
+    if(message.content == 'kys') {
+      message.reply('Alright, thats really rude.');
+    }
+});
+
+client.login('process.env.TOKEN'); //make the bot appear

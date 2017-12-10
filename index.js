c6295177db5d80b36e710ae4131dcd2b5795a0c8
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-10 19:38:03 +0000

    added mario command

--- index.js
+++ index.js
@@ -13,6 +13,38 @@ client.on('message', (message) => {
       message.reply('Celestia is da best pony!!/!!!!!');
     }
 
+    if(message.content == '!mario') {
+      var cuntmario = [
+        "░░░░░░░░░▓▓▓▓▀█░░░░░░░░░░░░░",
+        "░░░░░░▄▀▓▓▄██████▄ ",
+        "░░░░░▄█▄█▀░░▄░▄░█▀",
+        "░░░░▄▀░██▄░░▀░▀░▀▄ ",
+        "░░░░▀▄░░▀░▄█▄▄░░▄█▄ ",
+        "░░░░░░▀█▄▄░░▀▀▀█▀ ",
+        "░░░░░░█░░░░░░░░▄▀▀░▐",
+        "░░░░▄▀░░░░░░░░▐░▄▄▀ ",
+        "░░▄▀░░░▐░░░░░█▄▀░▐ ",
+        "░░█░░░▐░░░░░░░░▄░█ ",
+        "░░░█▄░░▀▄░░░░▄▀▐░█ ",
+        "░░░█▐▀▀▀░▀▀▀▀░░▐░█ ",
+        "░░▐█▐▄░░▀░░░░░░▐░█▄▄",
+        "░░░▀▀░GOTCHA░░░░▐▄▄▄▀"
+      ]
+      message.channel.send(cuntmario[0]);
+      message.channel.send(cuntmario[1]);
+      message.channel.send(cuntmario[2]);
+      message.channel.send(cuntmario[3]);
+      message.channel.send(cuntmario[4]);
+      message.channel.send(cuntmario[5]);
+      message.channel.send(cuntmario[6]);
+      message.channel.send(cuntmario[7]);
+      message.channel.send(cuntmario[8]);
+      message.channel.send(cuntmario[9]);
+      message.channel.send(cuntmario[10]);
+      message.channel.send(cuntmario[11]);
+      message.channel.send(cuntmario[12]);
+    }
+
     if(message.content == '!pony') {
       var voiceChannel = message.member.voiceChannel;
       voiceChannel.join().then(connection =>{

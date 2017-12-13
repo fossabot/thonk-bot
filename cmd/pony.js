67a893987b75ff1dbd95e2611210a71bfb8e9699
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-13 21:10:43 +0000

    added message

--- cmd/pony.js
+++ cmd/pony.js
@@ -1,5 +1,7 @@
 exports.run = (discord, client, message, args) => {
+  if (pony = false) return;
     var voiceChannel = message.member.voiceChannel;
+    message.reply('cancer activated, type t!pony again to cancel.')
       voiceChannel.join().then(connection =>{
         const dispatcher = connection.playFile('./media/shit.mp3') //this is a fucking mlp theme song if you wondering
         dispatcher.on("end", end => {

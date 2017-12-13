82481fb29082e4609f91bd8cf9fec82f31518a96
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-13 21:19:33 +0000

    added things

--- cmd/help.js
+++ cmd/help.js
@@ -6,7 +6,7 @@ exports.run = (discord, client, message, args) => {
           .setAuthor('TRIGGEREDDDDDDDDDDDD', 'https://cdn.discordapp.com/avatars/371630201521569792/ccd89854391742d0a5adf43fbc813471.png')
           .setDescription('List of commands')
           .setFooter('there is also some stupid respond, go find it by yourself')
-          .addField('t!help', 'ehhhhhh', true)
+          .addField('t!help', 'ehhhhhh, O O F', true)
           .addField('t!ping', 'ping the bot to see if the bot is online', true)
           .addField('t!pony', 'play a FUCKING mlp theme song on a channel that you currently on.', true)
           .addField('t!cringe', 'show a random image that will cringe your ass', true)

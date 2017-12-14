0a4cfccf7e5ba6419eb4e1f2dd9e9623955061be
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-14 21:49:03 +0000

    deleted respond

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const discord = require('discord.js'); //add discord.js idk
 const client = new discord.Client();
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
-
+var respondFile = require(`./responds.js`);
 
 client.on("ready", function() {
   console.log('Ready!');
@@ -22,29 +22,7 @@ client.on('message', (message) => {
   } catch (err) {
     console.error(err);
   }
-
-  if (message.content.startsWith('kys')) {
-    message.reply('fucking bitch stop being a faggot.')
-  }
-
-  if (message.content.startsWith('roblox')) {
-    message.reply('it just a fucking lego copy you cunt.')
-  }
-
-  if(message.content.startsWith('mtr')) {
-    message.reply(`can't find respond "mtr", you mean kcr?`)
-  } 
-
-  if(message.content.startsWith('wat')) {
-    message.reply('next time think twice before you type pls')
-  }
-
-  if(message.content.startsWith('kms')) {
-    message.reply({file: "https://cdn.discordapp.com/attachments/372799355385675777/390844751722708992/kms.png"})
-  }
 });
-
-
-client.login(process.env.BOT_TOKEN); //make the bot appear
+client.login('MzcxNjMwMjAxNTIxNTY5Nzky.DRQHSg.MGuT6EZonQvHrc2bU1cQS4OvQN4'); //make the bot appear
 //process.env.BOT_TOKEN
 

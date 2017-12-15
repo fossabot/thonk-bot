18e34fbaf428ed343d6a74997999b3d0115ddafe
TheSimpleBlock <mpalts.1234@hotmail.com>
2017-12-15 19:08:47 +0000

    added rnd also fixed ci

--- cmd/rnd.js
+++ cmd/rnd.js
@@ -1,5 +1,7 @@
 exports.run = (discord, client, message, args) => {
-    let [min, max] = args;
-    let rand = Math.floor(Math.random() * max) + min
-    message.channel.send(`Generated integer ${rand}`)
+    var [min, max] = args;
+    var rand = Math.floor(Math.random() * max);
+    var sum = parseFloat(rand) + parseFloat(min);
+    if (!min || !max) return message.reply('Invalid Usage, please do `t!roll <min num> <max num>`');
+    message.channel.send("Generated integer " + '`' + sum + '`')
 }

21a553df75b4ce2551e5ebc322a8be191b8bbb33
tsblock <mpalts1234@hotmail.com>
2017-12-26 18:20:48 +0800

    changed prefix

--- cmd/prune.js
+++ cmd/prune.js
@@ -2,7 +2,7 @@ exports.run = (discord, client, message, args) => {
     let num = args[0];
     if (isNaN(num)) {
         return message.reply('that\'s not a number.')
-    } else if (num < 2 || num > 1000) {
+    } else if (num < 2 || num > 100) {
         return message.reply('please enter a number between 2 and 100')
     } else if (!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) {
         return message.reply('I don\'t have these following permission: `MANAGE_MESSAGES`')

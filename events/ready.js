fa3abe2084fb926b2b9928ab298039467d8631dd
tsblock <16498864+tsblock@users.noreply.github.com>
2018-01-23 17:59:32 +0800

    lol k

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 exports.run = (client) => {
     console.log('Ready!');
-    client.user.setPresence({ game: { name: `HEIL FUHRER!!!!`}})
+    client.user.setPresence({ game: { name: `your mum`, type: 3}})
     //pasta.sync(); //sync the database
 }

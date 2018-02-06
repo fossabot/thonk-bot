f12da116c3f3433edec70d53b31eb7f4df6e113c
tsblock <tsb@localhost.localdomain>
2018-02-06 21:03:58 +0800

    en̉ḑ͛̿̉͗ m̩͙̫ͧ̈́̊̚͘͝y̧̮̝͛ ̰̻̓su̷͕͎̣ͪ̉ͫ̚f̘̘͌͌҉͇́̕̚͏f̩͚͔̘͐̑e̵̓ͤ͝҉҉͙̾r͓͇̗i̮ǹ̦̠̇g͊͟

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 exports.run = (client) => {
     console.log('Ready!');
-    client.user.setPresence({ game: { name: `your mum`, type: 3}})
+    client.user.setPresence({ game: { name: `@me for more info`, type: 3}})
     //pasta.sync(); //sync the database
 }

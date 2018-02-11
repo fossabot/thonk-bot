3c77ffd8f9e8c3c3fb7cd1b7608117dde8cd7eff
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-11 17:57:00 +0800

    fml

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 exports.run = (client) => {
     console.log('Ready!');
-    client.user.setPresence({ game: { name: `@me for more info`, ActivityType: 'WATCHING'}})
+    client.user.setPresence({game: {name: `on ${client.guilds.size} guilds | h.help`, type: 'WATCHING'}});
 }

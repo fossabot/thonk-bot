72d702111409b235ce7194ed4ec7163c5aff347d
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-12 21:48:53 +0800

    pranking command

--- cmd/say.js
+++ cmd/say.js
@@ -1,10 +1,11 @@
 module.exports = {
     name: 'say',
-    info: 'make the bot say something',
+    info: 'hahahah pranked hahaha',
     args: true,
     usage: '<message to say>',
     ownerOnly: true,
     execute(message, args){
+        message.delete()
         let messages = args.slice(0).join(" ")
         message.channel.send(messages)
     }

b28643904a23e6958383ff89391683e63423537f
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-12 19:47:07 +0800

    little fix

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const config = require('../cfg/config.js')
 exports.run = (client) => {
     console.log('Ready!');
-    client.user.setPresence({game: {name: `on ${client.guilds.size} guilds | ${config.prefix}.help`, type: 'WATCHING'}});
+    client.user.setActivity(`on ${client.guilds.size} guilds | ${config.prefix}.help`, { type: 'WATCHING' })
 }

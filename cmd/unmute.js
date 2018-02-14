a1e18c82840ae98847a7b6908b0755f5f5f58221
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-14 19:19:38 +0800

    stupid

--- cmd/unmute.js
+++ cmd/unmute.js
@@ -10,12 +10,11 @@ module.exports = {
     async execute (message, args) {
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('you don\'t have permission to use this command!')
         let toUnMute = message.guild.member(message.mentions.users.first());
-        let role = message.guild.roles.find(r => r.name === "h.mute");
+        let role = message.guild.roles.find(r => r.name === "thonk mute");
         const reason = args.slice(1).join(' ')
         if (!role || !toUnMute.roles.has(role.id))  return message.reply('this user is not muted!')
         await toUnMute.removeRole(role);
         message.channel.send(`✅ ${toUnMute} has been unmuted!`)
-        if (!message.guild.channels.find('topic', '-mod-log-')) return message.reply('Seems there\'s no mod log channel, I recommend you add a mod-log by adding `-mod-log-` as topic in your decided channel')
         var embed = new discord.RichEmbed()
             .setAuthor(message.author.username, message.author.displayAvatarURL)
             .setTitle('A member is muted!')

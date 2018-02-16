71819187f4c6982c8771630966906d99675fadee
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-16 12:43:35 +0800

    CLEVERBOT LOOLOLOL

---
+++ cmd/cleverbot.js
@@ -0,0 +1,22 @@
+const config = require('../cfg/config.js');
+const cleverbot = require('cleverbot.io');
+cb = new cleverbot(config.tokens.cleverbot_api.user, config.tokens.cleverbot_api.key)
+cb.setNick("85fa9ecb-f545-404a-a7e3-1756695d4021")
+module.exports = {
+    name: 'cleverbot',
+    aliases: ['cb', 'talk'],
+    info: 'Talk to cleverbot!',
+    args: true,
+    usage: '<message>',
+    execute(message, args) {
+        let toAsk = args.slice(0).join(" ");
+        cb.create(function(err, session) {
+            message.channel.send('Requesting... This might be take a while.').then(msg => {
+                cb.ask(toAsk, function(err, response) {
+                    msg.edit(response)
+                })
+            })
+
+        })
+    }
+}

dbcd8dd07df59c934c1b1fa8e20caa09adbaf4a8
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-20 14:28:25 +0800

    lol why tho

--- cmd/yesorno.js
+++ cmd/yesorno.js
@@ -1,5 +1,6 @@
 const request = require('superagent');
 const discord = require('discord.js');
+const moment = require('moment');
 module.exports = {
     name: 'yesorno',
     info: 'Yes or no?',
@@ -13,7 +14,8 @@ module.exports = {
         const embed = new discord.RichEmbed()
             .setTitle(`Answer: ${body.answer}`)
             .setImage(body.image)
-            .setColor('RANDOM');
+            .setColor('RANDOM')
+            .setFooter(`Requested by ${message.author.username} at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`, message.author.displayAvatarURL);
         message.channel.send(embed);
     },
 };

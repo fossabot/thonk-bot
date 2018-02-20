ddf575b1af70084ea106270eeba7f159ad023148
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-20 13:43:46 +0800

    THERE'S STILL ESLINT

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-
-
 const discord = require('discord.js'); //add discord.js idk
 const client = new discord.Client();
 const fs = require('fs');
 const respondFile = JSON.parse(fs.readFileSync('./cfg/responds.json', 'utf8'));
 const talkedRecently = new Set();
 const cmdFiles = fs.readdirSync('./cmd');
-const config = require('./cfg/config.js')
+const config = require('./cfg/config.js');
 client.commands = new discord.Collection();
 global.client = client;
 global.discord = discord;
 for (const file of cmdFiles) {
-  const cmd = require(`./cmd/${file}`)
-  client.commands.set(cmd.name, cmd)
+  const cmd = require(`./cmd/${file}`);
+  client.commands.set(cmd.name, cmd);
 }
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir('./events/', (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
-    let eventFunction = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
+    const eventFunction = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
     client.on(eventName, (...args) => eventFunction.run(client, ...args, respondFile, talkedRecently, config));
   });
 });
-
-
-
 client.login(config.tokens.bot); // nigger don't fucking leak token again
-
-

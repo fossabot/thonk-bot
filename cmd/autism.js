92a860e006541eedde607bdf61dd4a5f8969ff79
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-22 16:23:57 +0800

    shitpost

---
+++ cmd/autism.js
@@ -0,0 +1,16 @@
+const jeff = require('jimp');
+module.exports = {
+    name: 'autism',
+    usage: '[mention]',
+    async execute(message, args) { //eslint-disable-line no-unused-vars
+        const src = await jeff.read('./assets/template/autism.png');
+        let toChange = message.author.displayAvatarURL;
+        if (message.mentions.users.first()) toChange = message.mentions.users.first().displayAvatarURL;
+        const img = await jeff.read(toChange);
+        img.resize(498, 284);
+        src.composite(img, 1, 16)
+            .getBuffer(jeff.MIME_PNG, (err, buffer) => {
+                message.channel.send({ file: buffer, name:'autism.png' });
+            });
+    },
+};
---
+++ cmd/jack.js
@@ -0,0 +1,17 @@
+const jeff = require('jimp');
+module.exports = {
+    name: 'jack',
+    info: 'pls don\'t photoshop me',
+    usage: '[mention]',
+    async execute(message, args) { //eslint-disable-line no-unused-vars
+        const src = await jeff.read('./assets/template/jack.jpeg');
+        let toChange = message.author.displayAvatarURL;
+        if (message.mentions.users.first()) toChange = message.mentions.users.first().displayAvatarURL;
+        const img = await jeff.read(toChange);
+        img.resize(345, 258);
+        src.composite(img, 458, 356)
+            .getBuffer(jeff.MIME_PNG, (err, buffer) => {
+                message.channel.send({ file: buffer, name:'jack.png' });
+            });
+    },
+};

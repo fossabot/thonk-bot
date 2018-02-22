34bcc1eb2cb99a393e997f525d7ec5e115b972e3
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-22 15:00:14 +0800

    ez upvote

---
+++ cmd/ugly.js
@@ -0,0 +1,16 @@
+const jeff = require('jimp');
+module.exports = {
+    name: 'ugly',
+    usage: '[mention]',
+    async execute(message, args) { //eslint-disable-line no-unused-vars
+        const src = await jeff.read('./assets/template/ugly.png');
+        let toChange = message.author.displayAvatarURL;
+        if (message.mentions.users.first()) toChange = message.mentions.users.first().displayAvatarURL;
+        const image = await jeff.read(toChange);
+        image.resize(245, 361);
+        src.composite(image, 0, 0)
+            .getBuffer(jeff.MIME_PNG, (err, buffer) => {
+                message.channel.send({ file: buffer, name: 'ugly.png' });
+            });
+    },
+};
--- cmd/whodidthis.js
+++ cmd/whodidthis.js
@@ -9,7 +9,7 @@ module.exports = {
         let toChange = message.author.displayAvatarURL;
         if (message.mentions.users.first()) toChange = message.mentions.users.first().displayAvatarURL;
         const img = await jeff.read(toChange);
-        img.resize(816, 667); //yes it's 666 lolololokluoikliuok
+        img.resize(816, 667);
         src.composite(img, 32, 132)
             .getBuffer(jeff.MIME_PNG, (err, buffer) => {
                 message.channel.send({ file: buffer, name:'whodidthis.png' });

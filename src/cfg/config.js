54bd15b508d4b41032e58003221c9bde1a264dc9
tsblock <16498864+tsblock@users.noreply.github.com>
2018-02-23 18:03:53 +0800

    idk

---
+++ src/cfg/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+        prefix: 't.',
+        ownerID: '146078184260108288',
+        public: false,
+        tokens: {
+            bot: 'Mzk2MjA4MzkyMjE5MTMxOTA2.DWlq-w.-H6OPzD4OTz7NrTsQ2Ha6x4z9ko',
+            cleverbot_api: {
+                user: 'd67jrbLypRF1TJI3',
+                key: 'lwChGoT5sPyuI2BLBMMilnysjgJrbNlS',
+            },
+            nasa: 'xq4UrQUC7UxEKQprT1VaHsBA6Xe8b0yG8102ahJm',
+            dbl: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQxMjUxNjE5MjQwNjczMjgxMSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTE5MjAwOTc1fQ.fiWvHe8RUrx_OGHS6G1jCP1H94-atkRSKDt8aWyllU4',
+        },
+        items: {
+            price: {
+                1: 1500,
+                2: 5000,
+            },
+        },
+};

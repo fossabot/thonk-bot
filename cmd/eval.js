a893067310b291ef20bfb2503da73c0fd59df2ad
tsblock <16498864+tsblock@users.noreply.github.com>
2018-03-03 21:54:17 +0800

    nice meme

--- cmd/eval.js
+++ cmd/eval.js
@@ -1,24 +1,34 @@
+const request = require('superagent');
 module.exports = {
   name: 'eval',
   info: 'executes javascript code',
   usage: '<code>',
   args: true,
   ownerOnly: true,
-  execute(message, args) {
-    try {
+  async execute(message, args) {
     const clean = text => {
-            if (typeof(text) === 'string') //eslint-disable-line
-              return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
-            else return text; //eslint-disable-line
+      if (typeof text === 'string')
+        return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+      else
+          return text;
     };
-      const code = args.join('/ +/');
+    try {
+      const code = args.join(' ');
       let evaled = eval(code);
 
-      if (typeof evaled !== 'string') {evaled = require('util').inspect(evaled)} //eslint-disable-line
+      if (typeof evaled !== 'string')
+        evaled = require('util').inspect(evaled);
+      if (clean(evaled).length > 2000) {
+        const { body } = await request
+        .post('https://hastebin.com/documents')
+        .send(clean(evaled));
 
+        message.channel.send(`Since the result is over 2000 letters long, I have posted the result to hastebin! https://hastebin.com/${body.key}.js`);
+        return;
+      }
       message.channel.send(clean(evaled), { code:'xl' });
     } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``); //eslint-disable-line
+      message.channel.send(`\<:redtick:412529964945113100> **Error!** \`\`\`xl\n${clean(err.stack)}\n\`\`\``);
     }
   },
 };

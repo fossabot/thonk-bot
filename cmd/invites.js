a4b91bac3d7fc4a25ccc9ff5b385b7c73b9297f0
tsblock <16498864+tsblock@users.noreply.github.com>
2018-04-03 18:39:38 +0800

    minor fix to moneyleaderboard and also invite leaderboard HAIL TO OUR LEADERBOARD GOD

---
+++ cmd/invites.js
@@ -0,0 +1,29 @@
+const discord = require('discord.js'),
+table = require('table'),
+arraysort = require('array-sort');
+
+module.exports = {
+    name: 'invites',
+    info: 'Get the leaderboard of invites, useful for rewards member',
+    guildOnly: true,
+    async execute(message, args) {
+        let invites = await message.guild.fetchInvites().catch(() => {
+            return message.channel.send('<:redtick:412529964945113100> **I don\'t have permission to view invites!**');
+        });
+        invites = invites.array();
+
+        arraysort(invites, 'uses', { reverse: true });
+
+        const possibleInvites = [['Username', 'Uses']];
+        invites.forEach(invite => {
+            possibleInvites.push([invite.inviter.username, invite.uses])
+        });
+
+        const embed = new discord.RichEmbed()
+            .setColor('AQUA')
+            .setTitle('Server Invites')
+            .addField('Leaderboard', `\`\`\`${table.table(possibleInvites)}\`\`\``)
+        
+        message.channel.send(embed);
+    },
+};
--- cmd/moneyleaderboard.js
+++ cmd/moneyleaderboard.js
@@ -1,16 +1,20 @@
 const discord = require('discord.js'),
     table = require('table'),
-    db = require('quick.db');
+    db = require('quick.db'),
+    config = require('../cfg/config');
 
 module.exports = {
     name: 'moneyleaderboard',
     info: 'Check who\'s the richest!',
+    aliases: ['moneylb'],
     async execute(message, args) {
         const balances = await db.startsWith('balance', { sort: '.data' });
         let possibleBalance = [['Username', 'Balance']];
         balances.forEach(bal => {
             const [plsignore, userieedee] = bal.ID.split('_');
+            if (config.ownerID.includes(userieedee)) return;
             possibleBalance.push([client.users.get(userieedee).tag, bal.data]);
+            if (possibleBalance.length > 10) possibleBalance.slice(1).slice(-10);
         });
 
         const embed = new discord.RichEmbed()
